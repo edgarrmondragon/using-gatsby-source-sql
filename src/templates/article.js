98c75002959116ab533363056e8d56b750dbe436
Edgar@UbuntuLap <edgarrm358@gmail.com>
2019-01-13 21:16:47 -0600

    first commit

---
+++ src/templates/article.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default ({ data }) => {
+  return (
+    <Layout>
+      <div>
+        <h1>{data.article.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: data.article.fields.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    article(slug: { eq: $slug }) {
+      title
+      fields {
+        html
+      }
+    }
+  }
+`
